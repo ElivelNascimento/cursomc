d2c9b86cbbb5c4018bdba0d519c2618a9c37fe1f
Elivel Nascimento <elivelsantos81@gmail.com>
2020-07-06 22:14:53 -0300

    Testando a primeia classe de dominio - categoria

--- src/main/java/com/elivel/nascimento/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/elivel/nascimento/cursomc/resources/CategoriaResource.java
@@ -1,17 +1,30 @@
 package com.elivel.nascimento.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.elivel.nascimento.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() 
+	public List<Categoria> listar() 
 	{
-		return "REST está funcionando!";
+		Categoria cat1 = new Categoria(1, "Escritório");
+		Categoria cat2 = new Categoria(2, "TI");
+		
+		List<Categoria> lista = new ArrayList<>();
+		
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 
 }
