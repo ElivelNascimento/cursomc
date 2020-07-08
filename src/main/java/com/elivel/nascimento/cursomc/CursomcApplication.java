67c1d078c59fa1076b1286e4e8aba97ea9f10eb3
Elivel Nascimento <elivelsantos81@gmail.com>
2020-07-07 23:24:06 -0300

    Criando uma operacao de instancia

--- src/main/java/com/elivel/nascimento/cursomc/CursomcApplication.java
+++ src/main/java/com/elivel/nascimento/cursomc/CursomcApplication.java
@@ -1,15 +1,36 @@
 package com.elivel.nascimento.cursomc;
 
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.elivel.nascimento.cursomc.domain.Categoria;
+import com.elivel.nascimento.cursomc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 		
-		System.out.println("Desenvolvido por: ELivel Nascimento");
+			//System.out.println("Desenvolvido por: ELivel Nascimento");
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informatica");
+		Categoria cat2 = new Categoria(null, "TESTE");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
